c1e4d812c4304fbe759b65b6fe7659c4bf0e9896
apark9 <averypark@college.harvard.edu>
2024-12-26 12:11:54 -0500

    filtering excel files

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import code.sentiment as sentiment
 import code.graphing as graphing
 import code.climate as climate
 import code.financials as financials
+import code.models as models
 
 logging.basicConfig(
     filename="main.log",
@@ -12,29 +13,30 @@ logging.basicConfig(
 )
 
 if __name__ == "__main__":
-    logging.info("Starting main process...")
-    try:
-        sentiment.process_sentiment()
-        logging.info("Sentiment analysis completed successfully.")
-    except Exception as e:
-        logging.error(f"Error in sentiment analysis: {e}")
+    # logging.info("Starting main process...")
+    # try:
+    #     sentiment.process_sentiment()
+    #     logging.info("Sentiment analysis completed successfully.")
+    # except Exception as e:
+    #     logging.error(f"Error in sentiment analysis: {e}")
+
+    # try:
+    #     climate.run_climate_analysis()
+    #     logging.info("Climate analysis completed successfully.")
+    # except Exception as e:
+    #     logging.error(f"Error in graphing: {e}")
 
     try:
-        climate.run_climate_analysis()
-        logging.info("Climate analysis completed successfully.")
-    except Exception as e:
-        logging.error(f"Error in graphing: {e}")
-
-    try:
-        financials.run_financial_analysis()
+        # financials.run_financial_analysis()
+        models.run_models()
         logging.info("Financial analysis completed.")
     except Exception as e:
         logging.error(f"Error in financial analysis: {e}")
 
-    try:
-        graphing.perform_graphing()
-        logging.info("Graphing completed successfully.")
-    except Exception as e:
-        logging.error(f"Error in graphing: {e}")
+    # try:
+    #     graphing.perform_graphing()
+    #     logging.info("Graphing completed successfully.")
+    # except Exception as e:
+    #     logging.error(f"Error in graphing: {e}")
 
     logging.info("All processes completed.")
